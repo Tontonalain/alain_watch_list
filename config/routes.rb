afb9d44b08f5dd09421b09ad29399d80f89ac724
Alain Vallejo <alain.vallejo@laposte.net>
2022-12-07 07:46:21 -0400

    index list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  root to: "lists#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists do
+    resources :bookmarks, only: [:create, :new]
+  end
 end
